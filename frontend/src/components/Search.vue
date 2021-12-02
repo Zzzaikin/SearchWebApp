a8727291a845bb892750e409dd192db4562591d3
Ovezov <ovezov3210@gmail.com>
2021-12-02 17:53:46 +0300

    delete unused files

---
+++ frontend/src/components/Search.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <v-text-field label="Main input" outlined>
+      <v-btn slot="append" color="red"> mdi-plus </v-btn>
+    </v-text-field>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({}),
+};
+</script>
