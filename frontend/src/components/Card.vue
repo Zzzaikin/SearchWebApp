ebc27d26d811c13316719629c2b3fd5510136e9b
Ovezov <ovezov3210@gmail.com>
2021-12-03 14:02:32 +0300

    color change

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
-    <v-card class="mx-auto">
+    <v-card class="mx-auto" elevation="0" outlined>
       <v-card-text>
-        <v-row class="ma-0">
+        <v-row class="ma-0 my-3">
+          <p class="text-h4 text--primary">{{ title }}</p>
           <div class="image">
             <v-img
               max-height="120"
@@ -10,31 +11,25 @@
               src="https://picsum.photos/id/11/500/300"
             ></v-img>
           </div>
-          <p class="text-h4 text--primary ml-10">{{ title }}</p>
         </v-row>
         <div>
           <div class="parameters mt-4">
-            <p class="ma-0">
+            <v-row class="my-3 wr">
               <span class="parameter-title">Стадия готовности: </span>
-              <span class="ml-2">{{ completeStage }}</span>
-            </p>
-            <p class="ma-0">
+              <!--   <span class="ml-2">{{ completeStage }}</span> -->
+              <CompleteStageSlider></CompleteStageSlider>
+            </v-row>
+            <p class="my-3">
               <span class="parameter-title"
                 >Кейсы использования продукта:
               </span>
               <span class="ml-2">{{ useCases }}</span>
             </p>
-            <p class="ma-0">
+            <p class="my-3">
               <span class="parameter-title">Наличие пилотного проекта: </span>
               <span class="ml-2">{{ pilotProjectExists }}</span>
             </p>
-            <p class="ma-0">
-              <span class="parameter-title"
-                >Требуется ли сертификация продукта:
-              </span>
-              <span class="ml-2">{{ certificationNeed }}</span>
-            </p>
-            <p class="ma-0">
+            <p class="my-3">
               <span class="parameter-title">Польза продукта: </span>
               <span class="ml-2">{{ productBenefits }}</span>
             </p>
@@ -53,6 +48,7 @@
 </template>
 
 <script>
+import CompleteStageSlider from "./CompleteStageSlider.vue";
 import Filters from "./Filters.vue";
 export default {
   props: [
@@ -65,7 +61,7 @@ export default {
     "productBenefits",
     "shortDescription",
   ],
-  components: { Filters },
+  components: { Filters, CompleteStageSlider },
   data: () => ({}),
   methods: {
     goToApplication() {
@@ -78,5 +74,12 @@ export default {
 ::v-deep .v-card__text {
   font-size: 16px !important;
   color: black !important;
+}
+.image {
+  margin-left: 26px;
+}
+.wr {
+  display: flex;
+  flex-direction: row;
 }
 </style>
---
+++ frontend/src/components/CompleteStageSlider.vue
@@ -0,0 +1,33 @@
+
+
+
+
+<template>
+  <div>
+    <v-range-slider
+      :value="[0, 1]"
+      min="0"
+      max="3"
+      ticks="always"
+      tick-size="4"
+    >
+    </v-range-slider>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({}),
+};
+</script>
+
+
+
+
+
+
+
+
+
+
+
