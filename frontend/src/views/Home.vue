7db4dbfd6c1d4ac0d4129d165d2eda456b35940f
Ovezov <ovezov3210@gmail.com>
2021-12-03 17:29:40 +0300

    search

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,28 +3,38 @@
     <Search v-model="searchValue"> </Search>
     <div class="mt-10">
       <pre>searchValue:{{ searchValue }}</pre>
-      <div v-if="chunkedCards.length">
-        <v-row class="ma-0" v-for="(chunk, i) in chunkedCards" :key="i">
-          <v-col v-for="(item, j) in chunkedCards[i]" :key="item.id">
-            <Card
-              :class="j === 1 ? 'my-4 ml-2' : 'my-4 mr-2'"
-              :id="item.id"
-              :title="item.title"
-              :completeStage="item.completeStage"
-              :useCases="item.useCases"
-              :certificationNeed="item.certificationNeed"
-              :pilotProjectExists="item.pilotProjectExists"
-              :productBenefits="item.productBenefits"
-              :shortDescription="item.shortDescription"
-            ></Card>
-          </v-col>
-          <v-col v-if="chunkedCards[i].length < 2">
-            <v-spacer></v-spacer>
-          </v-col>
-        </v-row>
+      <div v-if="!loading">
+        <div v-if="chunkedCards.length">
+          <v-row class="ma-0" v-for="(chunk, i) in chunkedCards" :key="i">
+            <v-col v-for="(item, j) in chunkedCards[i]" :key="item.id">
+              <Card
+                :class="j === 1 ? 'my-4 ml-2' : 'my-4 mr-2'"
+                :id="item.id"
+                :title="item.title"
+                :completeStage="item.completeStage"
+                :useCases="item.useCases"
+                :certificationNeed="item.certificationNeed"
+                :pilotProjectExists="item.pilotProjectExists"
+                :productBenefits="item.productBenefits"
+                :shortDescription="item.shortDescription"
+              ></Card>
+            </v-col>
+            <v-col v-if="chunkedCards[i].length < 2">
+              <v-spacer></v-spacer>
+            </v-col>
+          </v-row>
+        </div>
+        <div v-else>
+          <span>Нет данных</span>
+        </div>
       </div>
       <div v-else>
-        <span>Нет данных</span>
+        <div class="spinner-wrapper">
+          <v-progress-circular
+            indeterminate
+            color="primary"
+          ></v-progress-circular>
+        </div>
       </div>
     </div>
   </div>
@@ -33,9 +43,14 @@
 <script>
 import Card from "../components/Card.vue";
 import Search from "../components/Search";
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 export default {
   name: "Home",
   data: () => ({
+    cards: [],
+    loading: false,
     searchValue: {
       text: "",
       filters: [],
@@ -45,17 +60,20 @@ export default {
     cards() {
       //return this.$store.state.applications;
       //return [];
-      return this.$store.getters.applicationsBySearchValue(this.searchValue);
+      //return this.$store.getters.applicationsBySearchValue(this.searchValue);
     },
     chunkedCards() {
-      return this.cards.chunk(2);
+      return this.cards.length > 0 ? this.cards.chunk(2) : [];
     },
-    watch: {
-      searchValue() {
-        this.cards = this.$store.getters.applicationsBySearchValue(
-          this.searchValue
-        );
-      },
+  },
+  watch: {
+    async searchValue() {
+      this.loading = true;
+      await sleep(400);
+      this.cards = this.$store.getters.applicationsBySearchValue(
+        this.searchValue
+      );
+      this.loading = false;
     },
   },
   components: {
@@ -67,5 +85,10 @@ export default {
 <style scoped>
 .col {
   padding: 0 !important;
+}
+.spinner-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
