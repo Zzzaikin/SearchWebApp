210db0220b8f08224b92b28b93088dea70afa309
Ovezov <ovezov3210@gmail.com>
2021-12-03 07:09:05 +0300

    filters

---
+++ frontend/src/components/Filters.vue
@@ -0,0 +1,156 @@
+
+
+
+
+<template>
+  <div class="filters">
+    <v-radio-group row class="ml-6" v-model="hasPilotProject">
+      <v-radio label="Нет пилотного проекта" :value="false"></v-radio>
+      <v-radio label="Есть пилотный проект" :value="true"></v-radio>
+    </v-radio-group>
+    <div class="filters">
+      <div class="filters-group" id="hasPilotProject" v-if="hasPilotProject">
+        <div class="filter-label ml-5">
+          <span>Заявка</span>
+        </div>
+        <v-row class="ma-0">
+          <v-col>
+            <v-select
+              outlined
+              class="filter mx-2"
+              :items="items"
+              :label="defaultLabel"
+            ></v-select>
+          </v-col>
+          <v-col>
+            <v-select
+              outlined
+              class="filter mx-2"
+              :items="items"
+              :label="defaultLabel"
+            ></v-select>
+          </v-col>
+          <v-col>
+            <v-select
+              outlined
+              class="filter mx-2"
+              :items="items"
+              :label="defaultLabel"
+            ></v-select>
+          </v-col>
+          <v-col>
+            <v-select
+              outlined
+              class="filter mx-2"
+              :items="items"
+              :label="defaultLabel"
+            ></v-select>
+          </v-col>
+        </v-row>
+        <div class="filter-label ml-5">
+          <span>Пилотный проект</span>
+        </div>
+        <v-row class="ma-0">
+          <v-col>
+            <v-select
+              outlined
+              class="filter mx-2"
+              :items="items"
+              :label="defaultLabel"
+            ></v-select>
+          </v-col>
+          <v-col>
+            <v-select
+              outlined
+              class="filter mx-2"
+              :items="items"
+              :label="defaultLabel"
+            ></v-select>
+          </v-col>
+          <v-col>
+            <v-select
+              outlined
+              class="filter mx-2"
+              :items="items"
+              :label="defaultLabel"
+            ></v-select>
+          </v-col>
+          <v-col>
+            <v-select
+              outlined
+              class="filter mx-2"
+              :items="items"
+              :label="defaultLabel"
+            ></v-select>
+          </v-col>
+        </v-row>
+      </div>
+      <div class="filters-group" id="doesntHasPilotProject" v-else>
+        <div class="filter-label ml-5">
+          <span>Заявка</span>
+        </div>
+        <v-row class="ma-0">
+          <v-col>
+            <v-select
+              outlined
+              class="filter mx-2"
+              :items="items"
+              :label="defaultLabel"
+            ></v-select>
+          </v-col>
+          <v-col>
+            <v-select
+              outlined
+              class="filter mx-2"
+              :items="items"
+              :label="defaultLabel"
+            ></v-select>
+          </v-col>
+          <v-col>
+            <v-select
+              outlined
+              class="filter mx-2"
+              :items="items"
+              :label="defaultLabel"
+            ></v-select>
+          </v-col>
+          <v-col>
+            <v-select
+              outlined
+              class="filter mx-2"
+              :items="items"
+              :label="defaultLabel"
+            ></v-select>
+          </v-col>
+        </v-row>
+      </div>
+    </div>
+    <div class="buttons pa-5">
+      <v-row class="ma-0">
+        <v-btn color="primary"> Применить </v-btn>
+        <v-btn outlined class="ml-6"> Сбросить </v-btn>
+      </v-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    hasPilotProject: false,
+    items: ["Значение 1", "Значение 2", "Значение 3"],
+    defaultLabel: "Фильтр",
+  }),
+};
+</script>
+
+
+
+
+
+
+
+
+
+
+
--- frontend/src/components/Search.vue
+++ frontend/src/components/Search.vue
@@ -1,26 +1,32 @@
 <template>
   <div>
     <div id="search-input">
-      <v-text-field label="Поиск" outlined v-model="text" hide-details>
-        <v-btn
-          slot="append"
-          :icon="true"
-          id="filter-btn"
-          @click="menuIsOpened = true"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-          >
-            <path
-              fill="currentColor"
-              fill-rule="evenodd"
-              d="M6.1 17.25a3 3 0 0 1 5.8 0h8.85a.75.75 0 1 1 0 1.5h-8.84a3 3 0 0 1-5.82 0H3.25a.75.75 0 1 1 0-1.5h2.84zm6-6a3 3 0 0 1 5.8 0h2.85a.75.75 0 1 1 0 1.5h-2.84a3 3 0 0 1-5.82 0H3.25a.75.75 0 1 1 0-1.5h8.84zm-6-6a3 3 0 0 1 5.8 0h8.85a.75.75 0 1 1 0 1.5h-8.84a3 3 0 0 1-5.82 0H3.25a.75.75 0 0 1 0-1.5h2.84zM9 7.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm6 6a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm-6 6a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"
-            ></path>
-          </svg>
-        </v-btn>
+      <v-text-field
+        label="Поиск"
+        outlined
+        v-model="text"
+        hide-details
+        prepend-inner-icon="mdi-magnify"
+      >
+        <div slot="append">
+          <v-btn :icon="true" id="filter-btn" @click="text = ''">
+            <v-icon> mdi-close </v-icon>
+          </v-btn>
+          <v-btn :icon="true" id="filter-btn" @click="menuIsOpened = true">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                fill-rule="evenodd"
+                d="M6.1 17.25a3 3 0 0 1 5.8 0h8.85a.75.75 0 1 1 0 1.5h-8.84a3 3 0 0 1-5.82 0H3.25a.75.75 0 1 1 0-1.5h2.84zm6-6a3 3 0 0 1 5.8 0h2.85a.75.75 0 1 1 0 1.5h-2.84a3 3 0 0 1-5.82 0H3.25a.75.75 0 1 1 0-1.5h8.84zm-6-6a3 3 0 0 1 5.8 0h8.85a.75.75 0 1 1 0 1.5h-8.84a3 3 0 0 1-5.82 0H3.25a.75.75 0 0 1 0-1.5h2.84zM9 7.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm6 6a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm-6 6a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"
+              ></path>
+            </svg>
+          </v-btn>
+        </div>
       </v-text-field>
     </div>
     <v-menu
@@ -30,55 +36,19 @@
       :open-on-click="false"
       :close-on-content-click="false"
     >
-      <v-sheet class="pa-4">
-        <div class="filters">
-          <v-row class="ma-0">
-            <v-col>
-              <v-select
-                outlined
-                class="filter mx-2"
-                :items="items"
-                :label="defaultLabel"
-              ></v-select>
-            </v-col>
-            <v-col>
-              <v-select
-                outlined
-                class="filter mx-2"
-                :items="items"
-                :label="defaultLabel"
-              ></v-select>
-            </v-col>
-            <v-col>
-              <v-select
-                outlined
-                class="filter mx-2"
-                :items="items"
-                :label="defaultLabel"
-              ></v-select>
-            </v-col>
-            <v-col>
-              <v-select
-                outlined
-                class="filter mx-2"
-                :items="items"
-                :label="defaultLabel"
-              ></v-select>
-            </v-col>
-          </v-row>
-        </div>
-      </v-sheet>
+      <v-sheet class="pa-4"> </v-sheet>
+      <Filters> </Filters>
     </v-menu>
   </div>
 </template>
 
 <script>
+import Filters from "./Filters.vue";
 export default {
+  components: { Filters },
   data: () => ({
     text: "",
     menuIsOpened: false,
-    items: ["Значение 1", "Значение 2", "Значение 3"],
-    defaultLabel: "Фильтр",
   }),
   methods: {
     log() {
@@ -95,8 +65,5 @@ export default {
 <style scoped>
 ::v-deep .v-text-field--enclosed .v-input__append-inner {
   margin-top: 10px !important;
-}
-.filter {
-  smax-width: 200px;
 }
 </style>
